be17833b3342327b0d7dfe6e45b382c0e5dd6825
siberiany <siberianheartty@gmail.com>
2025-12-30 00:08:57 +0200

    Update apie.js

--- src/apie.js
+++ src/apie.js
@@ -4,19 +4,19 @@ import As from "./images/as.png"; // Import the image
 function Apie() {
   return (
     <>
-          <h2 class="api">Apie mane</h2>
+          <h2 class="api">About Me</h2>
       <div className="apie">
         <p>
-          Aš esu grafikos dizainerė ir iliustratorė. Mano veikla apima reklamos,
-          brandingo dizainas. Aš taip pat užsiimu vizualizacijų animavimų ir
-          skaitmeniniu piešimu.<br/><br/>
-          Aš esu kilusi iš Lietuvos tačiau gyvenau ne vienoje šalyje. Taip aš
-          įgijau daug patirties bendraudama su įvairių kultūrų žmonėmis. Man
-          labai svarbu suprasti kliento poreikius ir išpildyti jo vizija bei
-          papildyti ją savo vaizduotės sugebėjimais. Mokymasis įvairiose dailės
-          bureliuose bei Vienožinskio dailės mokyklos baigimas padėjo man
-          patobulinti gėbėjimus dizaino srityje. Be to, savanoriaudama mokyklos
-          savivaldoje turėjau progą išmokti kurti reklamines vizualizacijas.
+          I am a digital artist who loves to create animated charaters and illustrations. I also have made frame-by-frame and spine animations as a hobby<br/><br/>
+          I am originally from Lithuania but have lived in other countries, which has given me a lot of experience 
+          in communicating with people from different cultures and backgrounds. 
+          I find it important to understand the client's needs 
+          and fulfill their vision by helping to enahnce it with my artistic skills.
+          Studying in various art schools and graduating from "Vienožinskis Art School" helped me to improve my design skills 
+          and gave me a basic understanding of things like proportion, colour, shape language and etc.
+          In addition, while volunteering in highschool student council, I had the opportunity to learn how to create advertising visualizations. 
+          After that I also attended a 2 year filmschool course in "Skalvijos Movie Academy" which enlightened me on the topic of film and visual storytelling.
+          In my University I have continued to improve my artistic skills while harboring knowledge from IT field.
         </p>
         <img
           src={As}
@@ -30,4 +30,5 @@ function Apie() {
 }
 
 export default Apie;
+
 
