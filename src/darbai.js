59a8b437905aa6263ee21b57433d4ac6e9c8625b
siberiany <siberianheartty@gmail.com>
2025-12-29 23:42:30 +0200

    Update darbai.js

--- src/darbai.js
+++ src/darbai.js
@@ -1,90 +1,76 @@
-import React, { Component } from "react";
+import { useEffect, useRef, useState } from "react";
 import darbas1 from "./images/darbas1.png";
 import darbas2 from "./images/darbas2.jpg";
 import darbas3 from "./images/darbas3.png";
 
-class Darbai extends Component {
-  constructor(props) {
-    super(props);
+const darbai = [
+  { src: darbas1, description: "Brandingo dizainas | Pakelio dizainas | Fizinis produktas" },
+  { src: darbas2, description: "Vizualinis dizainas | Skaitmeninė iliustracija" },
+  { src: darbas3, description: "Vizualinis dizainas | Skaitmeninė tapyba | Veikėjo dizainas" },
+];
 
-    this.state = {
-      currentIndex: 0,
-      darbai: [
-        { src: darbas1, description: "Brandingo dizainas | Pakelio dizainas | Fizinis produktas" },
-        { src: darbas2, description: "Vizualinis dizainas | Skaitmeninė iliustracija" },
-        { src: darbas3, description: "Vizualinis dizainas | Skaitmeninė tapyba | Veikėjo dizainas" },
-      ],
+export default function Darbai() {
+  const trackRef = useRef(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  /* center active item */
+  const scrollToIndex = (index) => {
+    const track = trackRef.current;
+    const item = track.children[index];
+
+    track.scrollTo({
+      left:
+        item.offsetLeft -
+        track.clientWidth / 2 +
+        item.clientWidth / 2,
+      behavior: "smooth",
+    });
+
+    setActiveIndex(index);
+  };
+
+  /* keyboard support */
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      if (e.key === "ArrowRight" && activeIndex < darbai.length - 1) {
+        scrollToIndex(activeIndex + 1);
+      }
+      if (e.key === "ArrowLeft" && activeIndex > 0) {
+        scrollToIndex(activeIndex - 1);
+      }
     };
-  }
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [activeIndex]);
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  handleKeyDown = (event) => {
-    if (event.key === "ArrowLeft") {
-      this.handlePrev();
-    } else if (event.key === "ArrowRight") {
-      this.handleNext();
-    }
-  };
-
-  handleNext = () => {
-    this.setState((prevState) => ({
-      currentIndex: (prevState.currentIndex + 1) % prevState.darbai.length,
-    }));
-  };
-
-  handlePrev = () => {
-    this.setState((prevState) => ({
-      currentIndex:
-        prevState.currentIndex === 0
-          ? prevState.darbai.length - 1
-          : prevState.currentIndex - 1,
-    }));
-  };
-
-  render() {
-    const { currentIndex, darbai } = this.state;
-    const currentDarbas = darbai[currentIndex];
-
-    return (
-      <div className="carousel-container">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src={currentDarbas.src}
-              alt="Darbas"
-              style={{
-                width: "35%",
-                height: "auto",
-              }}
-              className="carousel-image"
-            />
+  return (
+    <div className="carousel">
+      <div className="carousel-track" ref={trackRef}>
+        {darbai.map((item, i) => (
+          <div
+            key={i}
+            className={`carousel-item ${i === activeIndex ? "active" : ""}`}
+            onClick={() => scrollToIndex(i)}
+          >
+            <img src={item.src} alt="" />
           </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          onClick={this.handlePrev}
-        >
-          <i className="fa-solid fa-arrow-left"></i>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          onClick={this.handleNext}
-        >
-          <i className="fa-solid fa-arrow-right"></i>
-        </button>
-        <p>{currentDarbas.description}</p>
+        ))}
       </div>
-    );
-  }
+
+      <div className="controls">
+        <button onClick={() => scrollToIndex(Math.max(0, activeIndex - 1))}>
+          ←
+        </button>
+        <button onClick={() => scrollToIndex(Math.min(darbai.length - 1, activeIndex + 1))}>
+          →
+        </button>
+      </div>
+
+      <p className="description">{darbai[activeIndex].description}</p>
+    </div>
+  );
 }
 
 export default Darbai;
+
